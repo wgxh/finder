bfd54505feb6e3c891beb3b3cd19beed712d2ca5
wgxh-cli <15184439535@qq.com>
2021-06-06 11:50:03 +0800

    Add log feature

--- search.go
+++ search.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 )
 
-func Search(path string) func(query string) func() (*bool, *int) {
+func Search(path string) func(query string) func(log bool) (*bool, *int) {
 	matches := 0
 
 	finderCount := 1
@@ -13,8 +14,8 @@ func Search(path string) func(query string) func() (*bool, *int) {
 	searchRequest := make(chan string)
 	ok := false
 
-	return func(query string) func() (*bool, *int) {
-		search := func(path string) {
+	return func(query string) func(log bool) (*bool, *int) {
+		search := func(path string, log bool) {
 			files, err := ioutil.ReadDir(path)
 			if err == nil {
 				for _, file := range files {
@@ -23,6 +24,9 @@ func Search(path string) func(query string) func() (*bool, *int) {
 						searchRequest <- path + name + "/"
 					} else {
 						if name == query {
+							if log {
+								fmt.Println(path + name)
+							}
 							foundMatch <- true
 						}
 					}
@@ -30,12 +34,12 @@ func Search(path string) func(query string) func() (*bool, *int) {
 			}
 			findDone <- true
 		}
-		waitForSearch := func() {
+		waitForSearch := func(log bool) {
 			for {
 				select {
 				case path := <-searchRequest:
 					finderCount++
-					go search(path)
+					go search(path, log)
 				case <-foundMatch:
 					matches++
 				case <-findDone:
@@ -47,9 +51,9 @@ func Search(path string) func(query string) func() (*bool, *int) {
 				}
 			}
 		}
-		return func() (*bool, *int) {
-			go search(path)
-			go waitForSearch()
+		return func(log bool) (*bool, *int) {
+			go search(path, log)
+			go waitForSearch(log)
 
 			return &ok, &matches
 		}
